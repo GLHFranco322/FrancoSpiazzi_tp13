21cfd0f8749e92e935ee77c58fd1050760e918b0
Franco Spiazzi <francospiazzi96@gmail.com>
2024-11-07 10:47:20 -0300

    subo el tp

---
+++ bankBalance.js
@@ -0,0 +1,11 @@
+const operacionesBancarias = [10,10];
+
+let rnd = Match.floor(Match.random() * 200);
+let rnd_neg = Match.floor(Match.random() * 200);
+
+function calculateBalance(operacionesBancarias){
+    let depositos;
+    let retiros;
+
+
+}
